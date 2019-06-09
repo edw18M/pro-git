d367b3de4995e751cd6644f251e48b3b894cdd67
edw18M <1287175168@qq.com>
2019-06-09 14:25:48 +0800

    //该分支也令加一行注释，代表和master主分支同时改了该文件，模拟合并冲突

--- pro/src/main/java/com/it/pro/App.java
+++ pro/src/main/java/com/it/pro/App.java
@@ -14,6 +14,7 @@ public class App
 //        System.out.println( "Hello World!" );
 
 
+    	//该分支也令加一行注释，代表和master主分支同时改了该文件，模拟合并冲突
         AppService appService = new AppService();
         
         Method[] methods = appService.getClass().getMethods();
